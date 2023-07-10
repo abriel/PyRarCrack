7e58f534723a5c0abc1901d6a4be22e129c23b74
Dmitry Pisarev <sorra7@gmail.com>
2023-07-10 20:45:25 +0200

    unrardll engine: test the found password on all files in an archive     to avoid false-positive match on small files due to weak CRC32.

--- rarcracklib/engines/unrardll.py
+++ rarcracklib/engines/unrardll.py
@@ -1,13 +1,18 @@
 import unrardll
 
-def give_a_try(file_path, password):
+def give_a_try(file_path, password, fname=None):
     '''
     Testing a password using unrar API from libunrar.
     Requires /usr/lib/libunrar.so (libunrar)
     and unrardll python package (python3-unrardll on ubuntu / debian, python-unrardll on ArchLinux
     '''
+    if fname:
+        filename_selector = lambda x: x['filename'] == fname
+    else:
+        filename_selector = lambda x: True
+
     try:
-        unrardll.extract_member(file_path, lambda x: True, password)
+        unrardll.extract_member(file_path, filename_selector, password)
     except unrardll.BadPassword:
         return None
     except unrardll.unrar.UNRARError as e:
@@ -16,6 +21,10 @@ def give_a_try(file_path, password):
 
         raise
 
-    return password
+    if fname:
+        return password
+
+    if all((give_a_try(file_path, password, x) for x in unrardll.names(file_path))):
+        return password
 
 give_a_try.engine_name = 'unrardll'
